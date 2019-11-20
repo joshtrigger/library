30e0127e3616c6d23ac9396b7bfdebf27f6c617b
joshtrigger <lugjosh@gmail.com>
2019-11-20 15:58:12 +0300

    - added express server to serve our the app  - changes postinstall command

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const forceSSL = () => {
+  return function(req, res, next) {
+    if (req.headers['x-forward-proto'] !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    next();
+  };
+};
+const path = require('path');
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/library/index.html'));
+});
+
+app.use(forceSSL());
+
+app.use(express.static(__dirname + '/dist/library'));
+
+app.listen(process.env.PORT || 5000);
