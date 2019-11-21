72685ee083b34775c732cfefd2d4b47f8d6ef6b8
joshtrigger <lugjosh@gmail.com>
2019-11-22 00:26:37 +0300

    added interfaces for all data caollections

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { AuthService } from "../services/auth.service";
+import { Librarian } from "src/app/interfaces";
 
 @Component({
   selector: "app-login",
@@ -9,30 +10,39 @@ import { AuthService } from "../services/auth.service";
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  librarians: Array<Object>;
+  librarians: Array<Librarian>;
 
   constructor(private fb: FormBuilder, private authService: AuthService) {}
 
   ngOnInit() {
-    this.getAllLibrarians()
+    this.getAllLibrarians();
     const re = /\S+@\S+\.\S+/;
     this.loginForm = this.fb.group({
       email: ["", [Validators.required, Validators.pattern(re)]],
-      password: ["", [Validators.required]]
+      password: ["", [Validators.required, Validators.minLength(6)]]
     });
   }
 
-  getAllLibrarians():void{
+  getAllLibrarians(): void {
     this.authService.fetchLibrarians().subscribe(
-      users=>{
-        this.librarians=users
+      users => {
+        this.librarians = users;
       },
-      err=>{console.log(err)}
-    )
+      err => {
+        console.log(err);
+      }
+    );
   }
 
-  login():void {
-    const payload = this.loginForm.value;
-    
+  login(): any {
+    const { email, password } = this.loginForm.value;
+    for (const user of this.librarians) {
+      if (email === user.email && password === user.password) {
+        console.log("there was a match");
+        return true;
+      } else {
+        console.error("there was no match");
+      }
+    }
   }
 }
