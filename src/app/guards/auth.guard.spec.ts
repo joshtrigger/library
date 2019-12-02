86de24e6fb2c09704669588d4705a8e4423a761c
joshtrigger <lugjosh@gmail.com>
2019-12-02 16:56:31 +0300

    increased test coverage

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,73 +1,40 @@
 import { TestBed, async, inject } from "@angular/core/testing";
 
 import { AuthGuard } from "./auth.guard";
-import { RouterTestingModule } from "@angular/router/testing";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { AuthService } from "../auth/services/auth.service";
-import { Router } from "@angular/router";
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 
 describe("AuthGuard", () => {
-  let router: Router;
-  const routerStateSnapShop = {
-    url: ""
-  };
-  const activatedRouteSnapShot = {
-    url: [{}],
-    params: {},
-    data: {},
-    queryParams: {},
-    outlet: "primary",
-    fragment: null,
-    component: "",
-    routeConfig: {
-      canActivate: jasmine.createSpy(),
-      component: "",
-      path: ""
-    },
-    parent: null,
-    firstChild: null,
-    root: {},
-    children: {},
-    pathFromRoot: {},
-    queryParamMap: {}
-  };
+  let guard: AuthGuard;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule],
-      providers: [AuthGuard]
-    });
+  it("should activate route if user is logged in", () => {
+    const authServiceSpy = jasmine.createSpyObj("AuthService", [
+      "getCurrentUser"
+    ]);
+    const routerSpy = jasmine.createSpyObj("router", ["navigate"]);
+
+    guard = new AuthGuard(routerSpy, authServiceSpy);
+    authServiceSpy.getCurrentUser.and.returnValue(true);
+
+    guard.canActivate(new ActivatedRouteSnapshot(), {
+      url: "testUrl"
+    } as RouterStateSnapshot);
+
+    expect(guard.canActivate).toBeTruthy();
   });
 
-  it("should ...", inject([AuthGuard], (guard: AuthGuard) => {
-    expect(guard).toBeTruthy();
-  }));
+  it("should deactivate route if user is not logged in", () => {
+    const authServiceSpy = jasmine.createSpyObj("AuthService", [
+      "getCurrentUser"
+    ]);
+    const routerSpy = jasmine.createSpyObj("router", ["navigate"]);
 
-  it("should activate route if user is logged in", inject(
-    [AuthGuard],
-    (guard: AuthGuard) => {
-      const authServiceSpy = jasmine.createSpyObj("AuthService", [
-        "getCurrentUser"
-      ]);
-      authServiceSpy.getCurrentUser.and.returnValue(true);
+    guard = new AuthGuard(routerSpy, authServiceSpy);
+    authServiceSpy.getCurrentUser.and.returnValue(false);
 
-      guard.canActivate(activatedRouteSnapShot, routerStateSnapShop);
+    guard.canActivate(new ActivatedRouteSnapshot(), {
+      url: "testUrl"
+    } as RouterStateSnapshot);
 
-      expect(guard.canActivate).toBeTruthy();
-    }
-  ));
-
-  it("should deactivate route if user is not logged in", inject(
-    [AuthGuard],
-    (guard: AuthGuard) => {
-      const authServiceSpy = jasmine.createSpyObj("AuthService", [
-        "getCurrentUser"
-      ]);
-      authServiceSpy.getCurrentUser.and.returnValue(false);
-
-      guard.canActivate(activatedRouteSnapShot, routerStateSnapShop);
-
-      expect(guard.canActivate).toBeTruthy();
-    }
-  ));
+    expect(guard.canActivate).toBeTruthy();
+  });
 });
