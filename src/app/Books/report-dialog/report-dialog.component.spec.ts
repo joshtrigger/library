a206560f9bafada2cc02318426905468ff3541c4
joshtrigger <lugjosh@gmail.com>
2019-12-15 01:36:42 +0300

    fixed failing tests

--- src/app/Books/report-dialog/report-dialog.component.spec.ts
+++ src/app/Books/report-dialog/report-dialog.component.spec.ts
@@ -1,16 +1,34 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ReportDialogComponent } from './report-dialog.component';
+import { ReportDialogComponent } from "./report-dialog.component";
+import { MaterialModule } from "src/app/material.module";
+import { FormsModule } from "@angular/forms";
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from "@angular/material/dialog";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-describe('ReportDialogComponent', () => {
+describe("ReportDialogComponent", () => {
   let component: ReportDialogComponent;
   let fixture: ComponentFixture<ReportDialogComponent>;
+  const matDialogRefSpy = jasmine.createSpyObj("MatDialogRef", ["close"]);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReportDialogComponent ]
-    })
-    .compileComponents();
+      declarations: [ReportDialogComponent],
+      imports: [
+        MaterialModule,
+        FormsModule,
+        MatDialogModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: matDialogRefSpy },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +37,7 @@ describe('ReportDialogComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
