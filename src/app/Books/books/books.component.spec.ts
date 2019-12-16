8b73ac761223c4681d1abf52998305012f0223b1
joshtrigger <lugjosh@gmail.com>
2019-12-16 14:28:28 +0300

    writing tests

--- src/app/Books/books/books.component.spec.ts
+++ src/app/Books/books/books.component.spec.ts
@@ -1,28 +1,40 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { BooksComponent } from './books.component';
-import { MaterialModule } from 'src/app/material.module';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BooksComponent } from "./books.component";
+import { MaterialModule } from "src/app/material.module";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { BooksService } from '../books.service';
+import { of, throwError } from 'rxjs';
 
-describe('BooksComponent', () => {
+describe("BooksComponent", () => {
   let component: BooksComponent;
   let fixture: ComponentFixture<BooksComponent>;
+  let bookServiceSpy = jasmine.createSpyObj("BookService", [
+    "fetchBooks",
+    "lendOutBook",
+    "reportBook"
+  ]);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BooksComponent ],
-      imports:[MaterialModule, HttpClientTestingModule]
-    })
-    .compileComponents();
+      declarations: [BooksComponent],
+      imports: [MaterialModule],
+      providers:[{provide: BooksService, useValue:bookServiceSpy}]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BooksComponent);
     component = fixture.componentInstance;
+    bookServiceSpy.fetchBooks.and.returnValue(of([]))
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
+  it('should return error when calling fetchBooks function',()=>{
+    fixture.detectChanges()
+    bookServiceSpy.fetchBooks.and.returnValue(throwError({}))
+  })
 });
